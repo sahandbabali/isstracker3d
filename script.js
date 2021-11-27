d494ef2d42b6ed97906ab415d36c647794b1f3df
sahandbabali <sahand.babali@gmail.com>
2021-11-27 12:35:29 +0330

    changed rerendering

--- script.js
+++ script.js
@@ -9,35 +9,49 @@ var earth = new WE.map("earth_div", {
 
 WE.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(earth);
 
-var points = [];
 var lastmark;
-var markerr;
+var lastissicon;
+
 function fetchupdate() {
-  if (lastmark) {
-    lastmark.removeFrom(earth);
-  }
   fetch("http://api.open-notify.org/iss-now.json")
     .then((response) => response.json())
     .then((data) => {
-      points.push(data.iss_position);
-    })
-    .then(() => {
-      if (points.length != 0) {
-        for (let index = 0; index < points.length; index++) {
-          WE.marker(
-            [points[index].latitude, points[index].longitude],
-            "./marker.png",
-            64,
-            64
-          ).addTo(earth);
+      if (lastmark) {
+        // if it's not first fetch
+        //remove last iss icon
+        lastissicon.removeFrom(earth);
 
-          earth.setView([points[index].latitude, points[index].longitude]);
-        }
+        //add new red marker
         lastmark = WE.marker(
-          [
-            points[points.length - 1].latitude,
-            points[points.length - 1].longitude,
-          ],
+          [data.iss_position.latitude, data.iss_position.longitude],
+          "./marker.png",
+          64,
+          64
+        ).addTo(earth);
+
+        //add iss icon
+        lastissicon = WE.marker(
+          [data.iss_position.latitude, data.iss_position.longitude],
+          "./iss.png",
+          64,
+          64
+        ).addTo(earth);
+      } else {
+        // if it's first fetch
+        earth.setView([
+          data.iss_position.latitude,
+          data.iss_position.longitude,
+        ]);
+
+        lastmark = WE.marker(
+          [data.iss_position.latitude, data.iss_position.longitude],
+          "./marker.png",
+          64,
+          64
+        ).addTo(earth);
+
+        lastissicon = WE.marker(
+          [data.iss_position.latitude, data.iss_position.longitude],
           "./iss.png",
           64,
           64
@@ -52,8 +66,7 @@ const interval = setInterval(function () {
   fetchupdate();
 }, 10000);
 
-//
-
+// Clock code
 function startTime() {
   const today = new Date();
   let h = today.getHours();
@@ -64,12 +77,10 @@ function startTime() {
   document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
-
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
   } // add zero in front of numbers < 10
   return i;
 }
-
 startTime();
