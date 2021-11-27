7a14d6f7f2b981d8a7ee3684f0043ff4415548ad
sahandbabali <sahand.babali@gmail.com>
2021-11-27 09:41:02 +0330

    clock added

--- script.js
+++ script.js
@@ -53,3 +53,23 @@ const interval = setInterval(function () {
 }, 10000);
 
 //
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+startTime();
